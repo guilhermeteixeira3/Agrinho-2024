a4e7df1bb91209759777fa4f3972d0fab7e86b9d
guilhermeteixeira3 <109388751+guilhermeteixeira3@users.noreply.github.com>
2024-07-05 22:49:38 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+// Função para navegar até a página de projetos
+function navigateToProjects() {
+    window.location.href = 'page2.html';
+}
+
+// Adicionar event listeners para qualquer elemento interativo aqui
+document.addEventListener('DOMContentLoaded', () => {
+    // Código para ser executado quando a página carregar
+});
